307b57d8f2c03618d6b10138f885c67917b3cae3
wwartick <willwartick@gmail.com>
2022-10-08 14:52:33 -0700

    login/logout full stack

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
@@ -37,6 +37,14 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req,res) => {
+    if (req.session.loggedIn){
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
