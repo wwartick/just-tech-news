28c77fdf0a0b86dadc171c0c7c64d5299b7c0d05
wwartick <willwartick@gmail.com>
2022-10-08 03:50:58 -0700

    comment CRUD

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 //create associations
 User.hasMany(Post, {
@@ -37,6 +38,21 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey:'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
 
-module.exports = {User, Post, Vote };
+module.exports = {User, Post, Vote, Comment };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,24 +2,36 @@ const sequelize = require('../../config/connection');
 const router = require('express').Router();
 const { json } = require('body-parser');
 const { read } = require('fs');
-const {Post, User, Vote} = require('../../models');
+const {Post, User, Vote, Comment} = require('../../models');
 
 //get all users
 
 router.get('/', (req, res) => {
     Post.findAll({
-        where: { 
-            id:req.params.id
-        },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
         order: [['created_at', 'DESC']],
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
-            {
-                model: User,
-                attributes: ['username']
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
             }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
         ]
-    })
+       })
     .then(dbPostData => {
         if(!dbPostData){
             res.status(404).json({message: 'No post found with this id'});
